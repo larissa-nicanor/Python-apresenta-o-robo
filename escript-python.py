feb2afc4519ec355cce240ad38d6c4495c777cd5
Larissa Nicanor <larissa.nicanor@hotmail.com>
2025-04-26 14:23:47 -0300

    primeiro commit

---
+++ escript-python.py
@@ -0,0 +1,18 @@
+# Exercício 1:
+nome = input ('Qual é o seu nome?')
+prazer = input ('Ola' + nome + ' Prazer em te conhecer! Você é um robô?(Sim/Não)')
+print ()
+
+# Exercício 2:
+dia = input ('Vamos ao teste: Qual dia voce nasceu?')
+mes = input ('Sabe em qual mês voce nasceu?')
+ano = input ('Confirme o ano que voce nasceu?')
+print ('Voce nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + ' Correto?!')
+print ()
+
+# Exercício 3:
+pergunta = input ('Vamos ao teste de matematica, preparado?!(Sim)')
+a = input ('Digite um primeiro numero para somar')
+b = input ('Agora digite o segundo numero')
+soma = int(a) + int(b)
+print ('A soma é', soma)
